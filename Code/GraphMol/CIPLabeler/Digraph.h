22a2eda14124c69c761dd55766e054b9270bb6ad
Ric R <ricrogz@gmail.com>
2026-08-06 09:35:28 -0400

    Compact CIP digraph visit state

--- Code/GraphMol/CIPLabeler/Digraph.h
+++ Code/GraphMol/CIPLabeler/Digraph.h
@@ -95,8 +95,9 @@ class Digraph {
 
   void expand(Node *beg);
 
-  Node &addNode(std::vector<std::uint32_t> &&visit, Atom *atom,
-                boost::rational<int> &&frac, int dist, int flags);
+  Node &addNode(std::vector<std::uint64_t> &&visit, Atom *atom,
+                boost::rational<int> &&frac, int dist, int flags,
+                const Node *parent = nullptr);
 
   // Has `atom` been seen yet?
   bool seenAtom(Atom *atom) const;
